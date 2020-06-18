b027eb4544a2b2c6b868699f8706d602f1a2ea1e
axell-brendow <breno.axel@gmail.com>
2020-06-18 20:05:07 -0300

    Create /login route

--- index.js
+++ index.js
@@ -3,9 +3,31 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+const passport = require("passport");
+const saml = require("passport-saml");
+const fs = require("fs");
+
 const app = express();
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: "secret", resave: false, saveUninitialized: true }));
+
+app.get("/", (req, res) => {
+  res.send("Test Home Page");
+});
+
+app.get(
+  "/login",
+  (req, res, next) => {
+    console.log("-----------------------------");
+    console.log("/Start login handler");
+    next();
+  },
+  passport.authenticate("samlStrategy")
+);
+
+const server = app.listen(4300, () => {
+  console.log("Listening on port %d", server.address().port);
+});
